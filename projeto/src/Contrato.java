9d29620c9551d62d2a878e7caffa7d90d54982bf
Matheus Palermo <89110918+matheus1629@users.noreply.github.com>
2023-02-11 00:35:24 -0300

    Contrato

---
+++ projeto/src/Contrato.java
@@ -0,0 +1,27 @@
+public class Contrato implements Pagamento{
+    private Imovel imovel;
+    private Integer quantidadeParcelas;
+    private final Double IGPM = 3.79;
+    private Double jurosVenda;
+    private Double valorTotal;
+    private Corretor corretor;
+    private Cliente cliente;
+    private Double entrada;
+
+    public Contrato(ImovelVenda imovel, Integer quantidadeParcelas, Double jurosVenda, Double valorTotal, Corretor corretor, Cliente cliente) {
+        this.imovel = new ImovelVenda();
+        this.quantidadeParcelas = quantidadeParcelas;
+        this.jurosVenda = jurosVenda;
+        this.valorTotal = valorTotal;
+        this.corretor = corretor;
+        this.cliente = cliente;
+    }
+
+    public Contrato(ImovelAluguel imovel, Integer quantidadeParcelas, Double valorTotal, Corretor corretor, Cliente cliente) {
+        this.imovel = new ImovelAluguel();
+        this.quantidadeParcelas = quantidadeParcelas;
+        this.valorTotal = valorTotal;
+        this.corretor = corretor;
+        this.cliente = cliente;
+    }
+}
