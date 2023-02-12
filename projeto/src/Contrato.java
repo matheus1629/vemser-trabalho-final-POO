0a9bdcd66a5fcb5552aa696f1fdfcd06769816c7
Matheus Palermo <89110918+matheus1629@users.noreply.github.com>
2023-02-12 10:40:26 -0300

    Contrato

--- projeto/src/Contrato.java
+++ projeto/src/Contrato.java
@@ -1,5 +1,10 @@
-public class Contrato implements Pagamento{
-    private Imovel imovel;
+import java.util.List;
+
+public class Contrato implements Pagamento {
+
+
+    private ImovelVenda imovelVenda;
+    private ImovelAluguel imovelAluguel;
     private Integer quantidadeParcelas;
     private final Double IGPM = 3.79;
     private Double jurosVenda;
@@ -8,20 +13,53 @@ public class Contrato implements Pagamento{
     private Cliente cliente;
     private Double entrada;
 
-    public Contrato(ImovelVenda imovel, Integer quantidadeParcelas, Double jurosVenda, Double valorTotal, Corretor corretor, Cliente cliente) {
-        this.imovel = new ImovelVenda();
+    public Contrato(ImovelVenda imovelVenda, Integer quantidadeParcelas, Double jurosVenda, Double valorTotal, Corretor corretor, Cliente cliente) {
+        this.imovelVenda = new ImovelVenda();
         this.quantidadeParcelas = quantidadeParcelas;
         this.jurosVenda = jurosVenda;
         this.valorTotal = valorTotal;
         this.corretor = corretor;
         this.cliente = cliente;
+        calcularValorTotal();
     }
 
-    public Contrato(ImovelAluguel imovel, Integer quantidadeParcelas, Double valorTotal, Corretor corretor, Cliente cliente) {
-        this.imovel = new ImovelAluguel();
+    public Contrato(ImovelAluguel imovelAluguel, Integer quantidadeParcelas, Double valorTotal, Corretor corretor, Cliente cliente) {
+        this.imovelAluguel = new ImovelAluguel();
         this.quantidadeParcelas = quantidadeParcelas;
         this.valorTotal = valorTotal;
         this.corretor = corretor;
         this.cliente = cliente;
+        calcularValorTotal();
+    }
+
+
+    @Override
+    public Double calcularValorTotal() {
+        if (imovelVenda != null) {
+            return valorTotal = imovelVenda.getValorImovel() * (jurosVenda / 100);
+        } else {
+            if (quantidadeParcelas >= 12) {
+                Integer quantParcelasComJurosAnual = quantidadeParcelas - 12;
+                Double valorJurosTotal = quantParcelasComJurosAnual * imovelVenda.getValorImovel() * (IGPM / 100);
+                return valorTotal = imovelAluguel.getValorAluguel() + valorJurosTotal;
+            } else {
+                return valorTotal = imovelAluguel.getValorAluguel() * quantidadeParcelas;
+            }
+        }
+    }
+
+    public void setCorretor(Corretor corretor) {
+        this.cliente = cliente;
+    }
+    public Corretor getCorretor() {
+        return this.corretor;
+    }
+
+    public Double getValorTotal() {
+        return this.valorTotal;
+    }
+
+    public Double getEntrada() {
+        return entrada;
     }
 }
