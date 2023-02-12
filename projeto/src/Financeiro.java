108207207c0a2c548b67d0c4f7150a946452e4e5
Matheus Palermo <89110918+matheus1629@users.noreply.github.com>
2023-02-12 15:23:23 -0300

    Financeiro

--- projeto/src/Financeiro.java
+++ projeto/src/Financeiro.java
@@ -1,7 +1,5 @@
-import java.util.List;
-
 public interface Financeiro {
-    Double calcularComissao(String corretor);
+    Double calcularTotalComissao();
 
     Double calcularContasAReceber();
 }
