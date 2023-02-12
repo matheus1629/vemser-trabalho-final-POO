102e72692c3be92300713450bf37f3c9725ddcfd
Matheus Palermo <89110918+matheus1629@users.noreply.github.com>
2023-02-12 10:40:14 -0300

    Administracao

--- projeto/src/Administracao.java
+++ projeto/src/Administracao.java
@@ -1,14 +1,38 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class Administracao {
+public class Administracao implements Financeiro {
     private List<ImovelAluguel> listaImoveisAluguel = new ArrayList<>();
     private List<ImovelVenda> listaImoveisVenda = new ArrayList<>();
+    private List<Contrato> listaContratos = new ArrayList<>();
     private Double comissao; //fazer método pegando o valorTotal de ImovelVenda e ImovelAluguel e aplicar a comissão para cada tipo.
     private Double contasAReceber; // (quantidade de parcelas a serem recebidas)
-    private List<Contrato> listaContratos = new ArrayList<>();
 
+    @Override
+    public Double calcularComissao(String corretor) { //
+        List<Contrato> listaComisao = listaContratos.stream()
+                .filter(corretorFiltro -> corretorFiltro.getCorretor().getNome() == corretor)
+                .toList();
 
+        for (Contrato listaValorComissao : listaComisao) {
+            this.comissao += listaValorComissao.getValorTotal();
+        }
+
+        return this.comissao;
+    }
+
+    @Override
+    public Double calcularContasAReceber() {
+        Double totalVendido = 0.0;
+        for (Contrato listaValorTotal : listaContratos) {
+            totalVendido += listaValorTotal.getValorTotal();
+        }
+
+        Double totalEntrada = 0.0;
+        for (Contrato listaValorTotal : listaContratos) {
+            totalEntrada += listaValorTotal.getEntrada();
+        }
+
+        return this.contasAReceber = totalVendido - totalEntrada;
+    }
 }
